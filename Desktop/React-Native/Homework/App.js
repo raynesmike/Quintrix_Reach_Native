a9865d0536839c254e678f10007a1783679a3338
Michael Raynes <raynesmike@yahoo.com>
2022-03-06 17:18:16 -0800

    HW4 v.1

--- Desktop/React-Native/Homework/App.js
+++ Desktop/React-Native/Homework/App.js
@@ -17,48 +17,80 @@ export default class App extends Component<Props> {
   }
 }
 
-const BoxesContainer = () => (
+const BoxesContainer = (variation = 'default') => (
   <ScrollView contentContainerStyle={styles.container}>
-    <View style={[styles.yellowRectangle, { borderStyle: 'solid' }]}></View>
-    <View style={[styles.yellowRectangle, { borderStyle: 'dashed' }]}></View>
-    <View
-      style={[styles.yellowRectangle, { borderWidth: 7, borderColor: 'red' }]}
-    ></View>
-    <View
-      style={[
-        styles.yellowRectangle,
-        { borderStyle: 'dotted', borderColor: 'turquoise' },
-      ]}
-    ></View>
-    <View style={styles.yellowRectangle}>
-      <Text style={styles.boxTextStyle}>
-        Here we go, how it's going. There we go...
-      </Text>
-    </View>
-    <View style={styles.yellowRectangle}></View>
-    <View style={styles.yellowRectangle}></View>
-    <View style={styles.yellowRectangle}></View>
-    <View style={styles.yellowRectangle}></View>
-    <View style={styles.yellowRectangle}></View>
+    <View style={styles.variation1}></View>
+    <View style={styles.variation2}></View>
+    <View style={styles.variation3}></View>
+    <View style={styles.variation4}></View>
+    <Text style={[styles.default, styles.boxTextStyle]}>
+      Here we go, how it's going. There we go...
+    </Text>
+    <View style={styles.default}></View>
+    <View style={styles.default}></View>
+    <View style={styles.default}></View>
+    <View style={styles.default}></View>
+    <View style={styles.default}></View>
   </ScrollView>
 );
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  yellowRectangle: {
+  default: {
     backgroundColor: 'yellow',
     width: '80%',
     height: 50,
+    marginVertical: 30,
     borderWidth: 3,
     borderColor: 'lightgreen',
+    borderRadius: 0,
+  },
+  variation1: {
+    backgroundColor: 'yellow',
+    width: '80%',
+    height: 50,
     marginVertical: 30,
-    borderRadius: 0.1,
+    borderWidth: 3,
+    borderColor: 'lightgreen',
+    borderRadius: 1,
+    borderStyle: 'solid',
+  },
+  variation2: {
+    backgroundColor: 'yellow',
+    width: '80%',
+    height: 50,
+    marginVertical: 30,
+    borderWidth: 3,
+    borderColor: 'lightgreen',
+    borderRadius: 1,
+    borderStyle: 'dashed',
+  },
+  variation3: {
+    backgroundColor: 'yellow',
+    width: '80%',
+    height: 50,
+    marginVertical: 30,
+    borderWidth: 10,
+    borderColor: 'red',
+    borderRadius: 0,
+  },
+  variation4: {
+    backgroundColor: 'yellow',
+    width: '80%',
+    height: 50,
+    marginVertical: 30,
+    borderWidth: 4,
+    borderColor: 'turquoise',
+    borderRadius: 1,
+    borderStyle: 'dotted',
   },
   boxTextStyle: {
-    fontSize: 16,
+    flex: 1,
+    fontSize: 17,
+    paddingTop: 13,
     textAlign: 'center',
+    justifyContent: 'center',
   },
 });
